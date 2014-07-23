ef2734de9f2d96bc9f9862278d932a5ea7e9988c
Alejandro Lopez Osornio <alopez@termmed.com>
2014-07-23 14:42:42 -0300

    Refactoring to process module by module 7

--- src/main/java/org/ihtsdo/json/Transformer.java
+++ src/main/java/org/ihtsdo/json/Transformer.java
@@ -554,9 +554,7 @@ public class Transformer {
 					continue;
 				}
 				String[] columns = line.split("\\t");
-                if (concepts.get(Long.parseLong(columns[5])) == null) {
-                    System.out.println("Concept not found: " + Long.parseLong(columns[5]));
-                } else if (columns[2].equals("1") && concepts.get(Long.parseLong(columns[5])).getModule().equals(module)) {
+                if (columns[2].equals("1")) {
 					LightRefsetMembership loopMember = new LightRefsetMembership();
 					loopMember.setType(LightRefsetMembership.RefsetMembershipType.ATTRIBUTE_VALUE.name());
 					loopMember.setUuid(UUID.fromString(columns[0]));
