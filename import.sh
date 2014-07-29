0bf2711e383559b611137025efe1600f1ca08297
Alejandro Lopez Osornio <alopez@termmed.com>
2014-07-29 17:43:34 -0300

    Adding sh import script

---
+++ import.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Import json data
+# import.sh database collection
+mongo --eval "db = db.getSiblingDB('$1');db.dropDatabase();"
+mongoimport --file concepts.json -d $1 -c v$2
+mongoimport --file text-index.json -d $1 -c v$2tx
+mongo --eval "db = db.getSiblingDB('$1');db.v$2.ensureIndex({'conceptId' : 1});db.v$2.ensureIndex({'relationships.target.conceptId' : 1,'relationships.type.conceptId' : 1});db.v$2.ensureIndex({'statedRelationships.target.conceptId' : 1,'statedRelationships.type.conceptId' : 1});db.v$2.ensureIndex({'memberships.refset.conceptId' :1});"
+mongo --eval "db = db.getSiblingDB('$1');db.v$2tx.ensureIndex({'descriptionId' : 1});db.v$2tx.ensureIndex({term: 'text'});db.v$2tx.ensureIndex({term: 1});db.v$2tx.ensureIndex({words: 1});"
+echo "Done!"
