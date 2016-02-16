a1d23cb78e6ec38cf4d811b230496c5e039cb641
Rory Davidson <rda@ihtsdo.org>
2016-02-16 10:00:03 +0000

    Updated after further testing

--- scripts/import.sh
+++ scripts/import.sh
@@ -2,12 +2,13 @@
 # Import Snomed-CT json data
 #
 # Requires two arguments:
-# $ ./import.sh <database> <collection>
+# $ ./import.sh <host> <database> <collection>
+# e.g. ./import.sh localhost en-edition 20160131
 #
 
-if [[ $# -ne 2 ]]; then
-    echo 'Import script requires two arguments'
-    echo ' $ ./import.sh <database> <collection>'
+if [[ $# -ne 3 ]]; then
+    echo 'Import script requires three arguments'
+    echo ' $ ./import.sh <host> <database> <collection>'
     exit 1
 fi
 
@@ -20,49 +21,41 @@ function exitIfError {
     fi
 }
 
-ERROR=$(mongo --eval "db = db.getSiblingDB('server');db.resources.remove({'databaseName': '$1', 'collectionName': '$2'});")
-exitIfError ${ERROR}
-
-ERROR=$(mongo --eval "db = db.getSiblingDB('$1');db.dropDatabase();")
-exitIfError ${ERROR}
-
-ERROR=$(mongoimport --file concepts.json -d $1 -c v$2)
-exitIfError ${ERROR}
-
-ERROR=$(mongoimport --file text-index.json -d $1 -c v$2tx)
-exitIfError ${ERROR}
-
-ERROR=$(mongoimport --file manifest.json -d server -c resources)
-exitIfError ${ERROR}
+mongo --host $1 --eval "db = db.getSiblingDB('server');db.resources.remove({'databaseName': '$2', 'collectionName': '$3'});"
+mongo --host $1 --eval "db = db.getSiblingDB('$2');db.dropDatabase();"
+mongoimport --host $1 --file concepts.json -d $2 -c v$3
+mongoimport --host $1 --file text-index.json -d $2 -c v$3tx
+mongoimport --host $1 --file manifest.json -d server -c resources
 
 if [ -s statedTransitiveClosure.json ]; then
-    ERROR=$(mongoimport --file statedTransitiveClosure.json -d $1 -c v$2stc)
+    ERROR=$(mongoimport --file statedTransitiveClosure.json -d $2 -c v$3stc)
     exitIfError ${ERROR}
 fi
 
 if [ -s inferredTransitiveClosure.json ]; then
-    ERROR=$(mongoimport --file inferredTransitiveClosure.json -d $1 -c v$2itc)
+    ERROR=$(mongoimport --file inferredTransitiveClosure.json -d $2 -c v$3itc)
     exitIfError ${ERROR}
 fi
 
-+mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3.ensureIndex({'relationships.target.conceptId' : 1,'relationships.type.conceptId' : 1});"
- +mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3.ensureIndex({'statedRelationships.target.conceptId' : 1,'statedRelationships.type.conceptId' : 1});"
- +mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3.ensureIndex({'additionalRelationships.target.conceptId' : 1,'additionalRelationships.type.conceptId' : 1});"
- +mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3.ensureIndex({'memberships.refset.conceptId' :1});"
- +mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3.ensureIndex({inferredAncestors:1});"
- +mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3.ensureIndex({statedAncestors:1});"
- +mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3.ensureIndex({'statedRelationships.typeInferredAncestors':1});"
- +mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3.ensureIndex({'statedRelationships.typeStatedAncestors':1});"
- +mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3.ensureIndex({'relationships.typeInferredAncestors':1});"
- +mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3.ensureIndex({'relationships.typeStatedAncestors':1});"
- +mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3.ensureIndex({'statedRelationships.targetInferredAncestors':1});"
- +mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3.ensureIndex({'statedRelationships.targetStatedAncestors':1});"
- +mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3.ensureIndex({'relationships.targetInferredAncestors':1});"
- +mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3.ensureIndex({'relationships.targetStatedAncestors':1});"
- +mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3tx.ensureIndex({'descriptionId' : 1});"
- +mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3tx.ensureIndex({term: 'text'});"
- +mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3tx.ensureIndex({term: 1});"
- +mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3tx.ensureIndex({words: 1});"
+mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3.ensureIndex({'conceptId' : 1});"
+mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3.ensureIndex({'relationships.target.conceptId' : 1,'relationships.type.conceptId' : 1});"
+mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3.ensureIndex({'statedRelationships.target.conceptId' : 1,'statedRelationships.type.conceptId' : 1});"
+mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3.ensureIndex({'additionalRelationships.target.conceptId' : 1,'additionalRelationships.type.conceptId' : 1});"
+mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3.ensureIndex({'memberships.refset.conceptId' :1});"
+mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3.ensureIndex({inferredAncestors:1});"
+mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3.ensureIndex({statedAncestors:1});"
+mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3.ensureIndex({'statedRelationships.typeInferredAncestors':1});"
+mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3.ensureIndex({'statedRelationships.typeStatedAncestors':1});"
+mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3.ensureIndex({'relationships.typeInferredAncestors':1});"
+mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3.ensureIndex({'relationships.typeStatedAncestors':1});"
+mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3.ensureIndex({'statedRelationships.targetInferredAncestors':1});"
+mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3.ensureIndex({'statedRelationships.targetStatedAncestors':1});"
+mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3.ensureIndex({'relationships.targetInferredAncestors':1});"
+mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3.ensureIndex({'relationships.targetStatedAncestors':1});"
+mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3tx.ensureIndex({'descriptionId' : 1});"
+mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3tx.ensureIndex({term: 'text'});"
+mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3tx.ensureIndex({term: 1});"
+mongo --host $1 --port 27017 --eval "db = db.getSiblingDB('$2');db.v$3tx.ensureIndex({words: 1});"
 
 echo "Import Completed"
 exit 0
